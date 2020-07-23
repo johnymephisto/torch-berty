a41bff351d28bae203ad2d799547a9b64b46383d
Johny Jose <johnymephisto@gmail.com>
2020-07-23 12:16:59 +0530

    Adding flit

---
+++ torch-berty/berty.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Torch Berty Project
+
+This module aims at bringing the power of bert to the masses with easy to use
+entry point to train and get a production ready model.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+"""
+
+__version__ = '0.1'
